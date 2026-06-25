4e8df4e1fb1646ebe7189c79bcb42affa0fe44dc
Nayanamathad97 <nayanamathad97@gmail.com>
2026-06-25 14:08:00 +0530

    up5

--- setup_host.py
+++ setup_host.py
@@ -7,7 +7,7 @@ def run(cmd):
 if shutil.which("nginx"):
     print("nginx is installed")
 else:
-    command="sudo apt-get update && suao apt-get install -y nginx"
+    command="sudo apt-get update && sudo apt-get install -y nginx"
     run(command)
     run("sudo mv ./nayanap.site /etc/nginx/sites-available/nayanap.site")
     run("sudo ln-sf /etc/nginx/sites-available/nayanap.site /etc/nginx/sites-enabled/nayanap.site")
